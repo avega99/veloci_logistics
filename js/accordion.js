d01c23433602a44ca495a2512bae932f540aacd9
Alejandro Vega <avega392@gmail.com>
2024-08-06 12:27:43 -0600

    completed services setion

--- js/accordion.js
+++ js/accordion.js
@@ -4,14 +4,18 @@ document.addEventListener('DOMContentLoaded', function () {
   accordionHeaders.forEach((header) => {
     header.addEventListener('click', function () {
       const content = this.nextElementSibling
+      const chevron = this.querySelector('img')
 
       if (content.style.height && content.style.height !== '0px') {
         content.style.height = '0px'
+        chevron.style.transform = 'rotate(0.0turn)'
       } else {
         document.querySelectorAll('.accordion-content').forEach((content) => {
           content.style.height = '0px'
+          chevron.style.transform = 'rotate(0.0turn)'
         })
         content.style.height = `${content.scrollHeight}px`
+        chevron.style.transform = 'rotate(0.5turn)'
       }
     })
   })
